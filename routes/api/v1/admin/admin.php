7918c93378bffc737ddd54f276d35540145192e0
ayaz <nortmuller92@gmail.com>
2025-05-13 16:51:47 +0300

    correct structure project (routs)

--- routes/api/v1/admin/admin.php
+++ routes/api/v1/admin/admin.php
@@ -1,10 +1,10 @@
 <?php
 
-use App\Http\Controllers\Api\v1\Admin\AdminAuthenticationController;
+use App\Http\Controllers\Api\v1\Admin\AdminLoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
-    Route::post('login', [AdminAuthenticationController::class, 'login']);
+    Route::post('login', [AdminLoginController::class, 'login']);
 
     require_once __DIR__ . '/dish.php';
     require_once __DIR__ . '/product.php';
