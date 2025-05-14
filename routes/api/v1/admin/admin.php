2277f16647b208e1b5d1fd29c7756cab9b90f730
ayaz <nortmuller92@gmail.com>
2025-05-14 03:30:53 +0300

    add image route

--- routes/api/v1/admin/admin.php
+++ routes/api/v1/admin/admin.php
@@ -1,11 +1,18 @@
 <?php
 
 use App\Http\Controllers\Api\v1\Admin\AdminLoginController;
+use App\Http\Controllers\Api\v1\Admin\Image\ImageUploadController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->prefix('admin')->group(function () {
+Route::prefix('admin')->group(function () {
     Route::post('login', [AdminLoginController::class, 'login']);
 
-    require_once __DIR__ . '/dish.php';
-    require_once __DIR__ . '/product.php';
+    Route::middleware('auth:sanctum')->group(function () {
+        require_once __DIR__ . '/dish.php';
+        require_once __DIR__ . '/product.php';
+
+        Route::prefix('file')->group(function () {
+            Route::post('upload', [ImageUploadController::class, 'upload']);
+        });
+    });
 });
