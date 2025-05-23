5f4bfbd8f047f93da86262d9b5859237dc78b9d6
exaiter <19al968@gmail.com>
2025-05-23 12:45:08 +0300

    test generate

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 \Illuminate\Support\Facades\Route::get('/', function () {
 });
 
-Route::get('/generate-menu',   function () {return view('panel');});
+Route::get('/generate-menu',   function () {return view('generate');});
 Route::post('/generate-menu', [ \App\Http\Controllers\Api\v1\Admin\MenuGenerateController::class, 'generateMenu'])->name('generate-menu');
