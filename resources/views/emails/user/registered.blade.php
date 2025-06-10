1aec2d1e21fd5613edb8d17d4aa5ac8f4391103f
Егор Лисица <egor3k3@mail.ru>
2025-06-10 22:05:49 +0300

    Fix url in registered blade

--- resources/views/emails/user/registered.blade.php
+++ resources/views/emails/user/registered.blade.php
@@ -3,8 +3,8 @@
 
     Спасибо за регистрацию в {{ config('app.name') }}.
 
-    <x-mail::button :url="url(env('app_url'))">
-        Перейти на сайт
+    <x-mail::button :url="env('APP_URL')">
+    Перейти на сайт
     </x-mail::button>
 
     С уважением, {{ config('app.name') }}
