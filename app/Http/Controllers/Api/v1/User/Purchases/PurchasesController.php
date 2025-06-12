16ffd7e1447698c753a19b0bb60b91fbddfa8444
Егор Лисица <egor3k3@mail.ru>
2025-06-12 23:45:46 +0300

    Changed the receipt of products for purchases

--- app/Http/Controllers/Api/v1/User/Purchases/PurchasesController.php
+++ app/Http/Controllers/Api/v1/User/Purchases/PurchasesController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api\v1\User\Purchases;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\v1\User\GetAvailableProductsGroupedByDivisionRequest;
-use App\Models\Product\Product;
 use App\Models\User\UserProducts;
+use App\Services\Product\ProductServices;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -23,47 +23,13 @@ class PurchasesController extends Controller
         return response()->json($categories);
     }
 
-    public function products(GetAvailableProductsGroupedByDivisionRequest $request): JsonResponse
+    public function products(
+        GetAvailableProductsGroupedByDivisionRequest $request,
+        ProductServices $productServices
+    ): JsonResponse
     {
-        $existUserProductsIds = UserProducts::query()
-            ->where('users_id', auth()->id())
-            ->select(['uuid', 'users_id', 'product_id'])->get()
-            ->pluck('uuid')
-            ->toArray();
-
-        $products = Product::query()->where(function ($query) {
-            $query->where('users_id', auth()->id())
-                ->orWhereNull('users_id');
-        })
-            ->when($request->filled('name'), function ($query) use ($request) {
-                $name = trim($request->input('name'));
-                $query->where('name', 'LIKE', "%{$name}%");
-            })
-            ->when($request->filled('divisions_id'), function ($query) use ($request) {
-                $query->where('divisions_id', $request->input('divisions_id'));
-            })
-            ->whereNot('uuid', $existUserProductsIds)
-            ->select('name', 'image', 'uuid', 'divisions_id', 'unit_id')
-            ->with(['division', 'unit'])
-            ->paginate($request->filled('per_page') ? (int)$request->input('per_page') : 5);
-
-        $filterProducts = [];
-
-        foreach ($products as $product) {
-                $filterProducts[$product->division?->name ?? ''][] = $product->toArray();
-        }
-
-        return response()->json([
-            'data' => $filterProducts,
-            'meta' => [
-                'current_page' => $products->currentPage(),
-                'last_page' => $products->lastPage(),
-                'per_page' => $products->perPage(),
-                'total' => $products->total(),
-                'next_page_url' => $products->nextPageUrl(),
-                'prev_page_url' => $products->previousPageUrl(),
-            ],
-        ]);
+        $result = $productServices->getAvailableProductsGroupedByDivision($request);
+        return  response()->json($result);
     }
 
     public function acceptPurchase(Request $request): JsonResponse
