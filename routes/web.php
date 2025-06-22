9e219ff5d34e517954c13b44c3be74998561ce0e
Alexandr Laptev <19al968@gmail.com>
2025-06-22 18:53:33 +0000

    Update file web.php

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,3 @@
 });
 
 Route::get('/generate-menu',   function () {return view('generate');});
-Route::post('/generate-menu', [ \App\Http\Controllers\Api\v1\Admin\MenuGenerateController::class, 'generateMenu'])->name('generate-menu');
